b986158c9cb0e59a186eafa0b4698b4f5e85e647
@mpaez1 <mpaez@ikatechsolutions.com>
2024-10-17 08:57:04 -0500

    add-changes

---
+++ src/config/0-Cliente-Configuracion/1-intros-config/2-intros-intro2-config.jsx
@@ -0,0 +1,39 @@
+// Configuraciones de estilo
+export const colorTitleIntro = "text-customColor6 sm:text-5xl";
+export const colorTextIntro = "text-customColor5 sm:text-lg";
+export const colorSocialIconBg = "bg-customColor6";
+export const colorSocialIconText = "text-slate-700";
+export const colorSocialIconHoverBg = "hover:bg-primary";
+export const colorSocialIconHoverText = "hover:text-customColor4";
+
+// Información de la barbería
+export const titleIntro = "Class Barber";
+export const textIntro = `
+    "No es solo una barbería, es una experiencia exclusiva para el caballero moderno que busca calidad y estilo 
+    en un ambiente elegante. Ofrecemos servicios de cortes de cabello, cejas, barba y más, brindamos una atención 
+    personalizada que asegura cada detalle. Ven y vive un servicio clásico diseñado para ti."
+`;
+
+// Links sociales
+export const socialLinks = [
+    {
+        text: 'Go to GitHub repo',
+        href: 'https://github.com/JustGoodUI/ovidius-astro-theme',
+        icon: 'facebook'
+    },
+    {
+        text: 'Follow on Instagram',
+        href: 'https://instagram.com/',
+        icon: 'instagram'
+    },
+    {
+        text: 'Follow on X',
+        href: 'https://twitter.com/justgoodui',
+        icon: 'x-twitter'
+    },
+    {
+        text: 'Follow on TikTok',
+        href: 'https://tiktok.com/',
+        icon: 'tiktok'
+    }
+];
