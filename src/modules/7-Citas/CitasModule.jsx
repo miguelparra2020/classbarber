07ca43a855cb59471ff657e04be586c3c0d219e7
@mpaez1 <mpaez@ikatechsolutions.com>
2024-11-11 21:20:19 -0500

    add-changes

--- src/modules/7-Citas/CitasModule.jsx
+++ src/modules/7-Citas/CitasModule.jsx
@@ -10,37 +10,50 @@ const CitasModule = () => {
     const [selectedBarbero, setSelectedBarbero] = useState();
     const [selectedServicio, setSelectedServicio] = useState();
     const [selectedFecha, setSelectedFecha] = useState();
+    const [selectDay, setSelectDay] = useState();
     const [selectedHorario, setSelectedHorario] = useState();
     const [disabledServices, setDisabledServices] = useState(false);
     const [disabledBarbero, setDisabledBarbero] = useState(false);
-    // Funciones para manejar los cambios en los estados de seleccion
-    console.log("selectedServicio:", selectedServicio)
-    console.log("selectedFecha", selectedFecha)
-    
     const [citaProgramming] = useState({
       servicio: selectedServicio,
       barbero: selectedBarbero,
       fecha: selectedFecha,
-      horario: selectedHorario
+      horario: selectedHorario 
     });
 
-    const arrayServicios = [
-        { nombre: "Corte de cabello", minutos: 30, id: 1 },
-        { nombre: "Arreglo de barba ", minutos: 20, id: 2 },
-        { nombre: "Cejas con navaja", minutos: 5, id: 3 },
-        { nombre: "Corte + Barba", minutos: 45, id: 4 },
-        { nombre: "Corte + Cejas", minutos: 40, id: 5 },
-        { nombre: "Corte + Barba + Cejas", minutos: 50, id: 6 },
-        { nombre: "Colorimetria", minutos: 90, id: 7 },
-        { nombre: "Delineado", minutos: 10, id: 8 },
+      const arrayServicios = [    
+          { nombre: "Corte de cabello", minutos: 30, id: 1 },
+          { nombre: "Arreglo de barba ", minutos: 15, id: 2 },
+          { nombre: "Cejas con navaja", minutos: 15, id: 3 },
+          { nombre: "Corte + Barba", minutos: 45, id: 4 },
+          { nombre: "Corte + Cejas", minutos: 45, id: 5 },
+          { nombre: "Corte + Barba + Cejas", minutos: 60, id: 6 },
+          { nombre: "Colorimetria", minutos: 90, id: 7 },
+          { nombre: "Delineado", minutos: 15, id: 8 },
       ]
 
-    const arrayBarberos = [
-        { img : "https://www.carloscondepeluqueros.com/wp-content/uploads/2019/01/cabecera-barbero-profesional.jpg",
-           nombre: "Carlos Garcia", id: 1 },
-        { img: "https://www.carloscondepeluqueros.com/wp-content/uploads/2019/01/cabecera-barbero-profesional.jpg",
-          nombre: "Juan Gomez", id: 2 },
-    ]
+      const arrayBarberos = [
+          { img : "https://www.carloscondepeluqueros.com/wp-content/uploads/2019/01/cabecera-barbero-profesional.jpg",
+            nombre: "Carlos Garcia", 
+            id: 1, 
+            diaNoDisponible: "DOM", 
+            Horario1: "10:00",  
+            Horario2: "14:00",
+            Horario3: "16:00",
+            Horario4: "21:00"
+          },
+          { img: "https://www.carloscondepeluqueros.com/wp-content/uploads/2019/01/cabecera-barbero-profesional.jpg",
+            nombre: "Juan Gomez", 
+            id: 2, 
+            diaNoDisponible: "MAR",
+            Horario1: "10:00",  
+            Horario2: "14:00",
+            Horario3: "16:00",
+            Horario4: "21:00"
+          },
+      ]
+    
+    const opcionesHorarias = ["10:00","10:30","11:00","11:30","12:00"]
     
     // Funciones para cambiar de paso
     const goToNextStep = () => {
@@ -79,33 +92,33 @@ const CitasModule = () => {
 
     const [dates, setDates] = useState(generateDates());
 
-  // Función para generar los próximos días a partir de hoy
-  function generateDates(days = 90) {
-    const today = new Date();
-    const result = [];
+    // Función para generar los próximos días a partir de hoy
+    function generateDates(days = 90) {
+      const today = new Date();
+      const result = [];
 
-    for (let i = 0; i < days; i++) {
-      const currentDate = new Date(today);
-      currentDate.setDate(today.getDate() + i);
-      result.push({
-        month: format(currentDate, 'MMM', { locale: es }).toUpperCase(),
-        day: format(currentDate, 'dd'),
-        weekday: format(currentDate, 'EEE', { locale: es }).toUpperCase()
-      });
+      for (let i = 0; i < days; i++) {
+        const currentDate = new Date(today);
+        currentDate.setDate(today.getDate() + i);
+        result.push({
+          month: format(currentDate, 'MMM', { locale: es }).toUpperCase(),
+          day: format(currentDate, 'dd'),
+          weekday: format(currentDate, 'EEE', { locale: es }).toUpperCase()
+        });
+      }
+      return result;
     }
-    return result;
-  }
 
     return (
         <div>
             <div className='flex flex-col items-center content-center'>
-        <h1 className="text-3xl md:text-3xl font-bold leading-tighter tracking-tighter mb-1 font-heading">
-          <div className="mt-1">
-            <span>Agendar cita</span>
-          </div>
-        </h1>
-        </div>
-        <div className="w-full">
+              <h1 className="text-3xl md:text-3xl font-bold leading-tighter tracking-tighter mb-1 font-heading">
+                <div className="mt-1">
+                  <span>Agendar cita</span>
+                </div>
+              </h1>
+            </div>
+            <div className="w-full">
                 <div className="grid max-w-xs grid-cols-3	 gap-1 p-1 mx-auto my-2 bg-gray-100 rounded-lg " role="group">
                     <button onClick={() => setCurrentStep(1)}  type="button" className={`flex flex-row items-center content-center 
                         text-center px-1 py-1.5 text-xs font-medium
@@ -139,89 +152,85 @@ const CitasModule = () => {
                     </button>
                 </div>
             </div>
-        
-           
 
             {/* Renderizado condicional de los pasos */}
             {currentStep === 1 && (
-    <div className="w-full flex flex-col items-center justify-center">
-        <div className="w-full max-w-sm p-1 bg-white border border-gray-200 rounded-lg shadow sm:p-8 ">
-            <div className="flex items-center justify-between mb-1">
-                <h5 className="text-xl font-bold leading-none text-gray-900 ">Servicios</h5>
-                <span className="text-sm font-medium text-gray-600 hover:underline ">
-                    Seleccionar servicio
-                </span>
-            </div>
-            <div className="flow-root">
-            <ul role="list" className="divide-y divide-gray-200 ">
-  {arrayServicios.map((servicio, index) => (
-    <li key={index} className="py-3 sm:py-4">
-      <label htmlFor={servicio.id || selectedServicio} onClick={() => {if(!disabledServices){
-        setSelectedServicio(servicio.id)
-        notifyServiceSelected(servicio.id, servicio.nombre)
-      }        
-      }} className="flex items-center cursor-pointer">
-        <div className="flex-shrink-0">
-          <IconGeneral
-            params={{
-              color: "currentColor",
-              size: "30",
-              className: servicio.iconoClase,
-              viewBox: "0 0 16 16",
-              path: "M3.5 3.5c-.614-.884-.074-1.962.858-2.5L8 7.226 11.642 1c.932.538 1.472 1.616.858 2.5L8.81 8.61l1.556 2.661a2.5 2.5 0 1 1-.794.637L8 9.73l-1.572 2.177a2.5 2.5 0 1 1-.794-.637L7.19 8.61zm2.5 10a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0m7 0a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0"
-            }}
-          />
-        </div>
-        <div className="flex-1 min-w-0 ms-4">
-          <p className="text-sm font-medium text-gray-900 truncate ">
-            {servicio.nombre}
-          </p>
-          <p className="text-sm text-gray-500 truncate flex flex-row">
-            <IconGeneral
-              params={{
-                color: "currentColor",
-                size: "16",
-                className: "bi bi-stopwatch-fill",
-                viewBox: "0 0 16 16",
-                path: "M6.5 0a.5.5 0 0 0 0 1H7v1.07A7.001 7.001 0 0 0 8 16a7 7 0 0 0 5.29-11.584l.013-.012.354-.354.353.354a.5.5 0 1 0 .707-.707l-1.414-1.415a.5.5 0 1 0-.707.707l.354.354-.354.354-.012.012A6.97 6.97 0 0 0 9 2.071V1h.5a.5.5 0 0 0 0-1zm2 5.6V9a.5.5 0 0 1-.5.5H4.5a.5.5 0 0 1 0-1h3V5.6a.5.5 0 1 1 1 0"
-              }}
-            /> &nbsp; {servicio.minutos} min
-          </p>
-        </div>
-        <div className="inline-flex items-center text-base font-semibold text-gray-900 ">
-        <input
-            id={servicio.id}
-            type="radio"
-            name="servicio"
-            className="w-4 h-4 text-gray-600 bg-gray-100 border-gray-300 rounded focus:ring-gray-500 "
-            checked={selectedServicio === servicio.id} // Esto mantiene el input marcado si coincide con el servicio seleccionado
-            onChange={() => setSelectedServicio(servicio.id)}
-            disabled={disabledServices}
-          />
-        </div>
-      </label>
-    </li>
-  ))}
-</ul>
+              <div className="w-full flex flex-col items-center justify-center">
+                  <div className="w-full max-w-sm p-1 bg-white border border-gray-200 rounded-lg shadow sm:p-8 ">
+                      <div className="flex items-center justify-between mb-1">
+                          <h5 className="text-xl font-bold leading-none text-gray-900 ">Servicios</h5>
+                          <span className="text-sm font-medium text-gray-600 hover:underline ">
+                              Seleccionar servicio
+                          </span>
+                      </div>
+                      <div className="flow-root">
+                      <ul role="list" className="divide-y divide-gray-200 ">
+            {arrayServicios.map((servicio, index) => (
+              <li key={index} className="py-3 sm:py-4">
+                <label htmlFor={servicio.id || selectedServicio} onClick={() => {if(!disabledServices){
+                  setSelectedServicio(servicio.id)
+                  notifyServiceSelected(servicio.id, servicio.nombre)
+                }        
+                }} className="flex items-center cursor-pointer">
+                  <div className="flex-shrink-0">
+                    <IconGeneral
+                      params={{
+                        color: "currentColor",
+                        size: "30",
+                        className: servicio.iconoClase,
+                        viewBox: "0 0 16 16",
+                        path: "M3.5 3.5c-.614-.884-.074-1.962.858-2.5L8 7.226 11.642 1c.932.538 1.472 1.616.858 2.5L8.81 8.61l1.556 2.661a2.5 2.5 0 1 1-.794.637L8 9.73l-1.572 2.177a2.5 2.5 0 1 1-.794-.637L7.19 8.61zm2.5 10a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0m7 0a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0"
+                      }}
+                    />
+                  </div>
+                  <div className="flex-1 min-w-0 ms-4">
+                    <p className="text-sm font-medium text-gray-900 truncate ">
+                      {servicio.nombre}
+                    </p>
+                    <p className="text-sm text-gray-500 truncate flex flex-row">
+                      <IconGeneral
+                        params={{
+                          color: "currentColor",
+                          size: "16",
+                          className: "bi bi-stopwatch-fill",
+                          viewBox: "0 0 16 16",
+                          path: "M6.5 0a.5.5 0 0 0 0 1H7v1.07A7.001 7.001 0 0 0 8 16a7 7 0 0 0 5.29-11.584l.013-.012.354-.354.353.354a.5.5 0 1 0 .707-.707l-1.414-1.415a.5.5 0 1 0-.707.707l.354.354-.354.354-.012.012A6.97 6.97 0 0 0 9 2.071V1h.5a.5.5 0 0 0 0-1zm2 5.6V9a.5.5 0 0 1-.5.5H4.5a.5.5 0 0 1 0-1h3V5.6a.5.5 0 1 1 1 0"
+                        }}
+                      /> &nbsp; {servicio.minutos} min
+                    </p>
+                  </div>
+                  <div className="inline-flex items-center text-base font-semibold text-gray-900 ">
+                  <input
+                      id={servicio.id}
+                      type="radio"
+                      name="servicio"
+                      className="w-4 h-4 text-gray-600 bg-gray-100 border-gray-300 rounded focus:ring-gray-500 "
+                      checked={selectedServicio === servicio.id} // Esto mantiene el input marcado si coincide con el servicio seleccionado
+                      onChange={() => setSelectedServicio(servicio.id)}
+                      disabled={disabledServices}
+                    />
+                  </div>
+                </label>
+              </li>
+            ))}
+          </ul>
 
 
-            </div>
-        </div>
-        <br />
-        <div className="w-screen gap-4 flex flex-row justify-center  items-center content-center">
-                        <button onClick={goToNextStep} disabled={!selectedServicio}  className="bg-customColor8 hover:bg-customColor5 text-white hover:text-gray-800 rounded font-bold py-2 px-4 flex items-center">Siguiente</button>
-                    </div>
+                      </div>
+                  </div>
+                  <br />
+                  <div className="w-screen gap-4 flex flex-row justify-center  items-center content-center">
+                                  <button onClick={goToNextStep} disabled={!selectedServicio}  className="bg-customColor8 hover:bg-customColor5 text-white hover:text-gray-800 rounded font-bold py-2 px-4 flex items-center">Siguiente</button>
+                              </div>
 
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-                    <br />
-    </div>
-
-)}
-
+                              <br />
+                              <br />
+                              <br />
+                              <br />
+                              <br />
+                              <br />
+              </div>
+            )}
 
             {currentStep === 2 && (
                 <div className="w-full flex flex-col items-center justify-center">
@@ -333,7 +342,10 @@ const CitasModule = () => {
                 ? 'bg-gray-800 text-white'
                 : 'bg-gray-200 text-gray-800 hover:bg-gray-800 hover:text-white'
             }`}
-            onClick={() => setSelectedFecha(date)}
+            onClick={() => {
+              setSelectedFecha(date)
+              setSelectDay(date.weekday)
+            }}
           >
             <div>
               <strong>{date.month}</strong>
@@ -353,6 +365,21 @@ const CitasModule = () => {
                       Horarios disponibles:
                     </div>
                     <br />
+                    <div>
+                      {
+                        selectDay === arrayBarberos[selectedBarbero-1].diaNoDisponible ? <>
+                         <strong>Día no disponible, barbero se encuentra en día de descanso.</strong> 
+                        </> : <>
+  <div>
+    {
+      opcionesHorarias.map((item, index) => (
+        <button key={index} className='bg-slate-200 rounded-sm p-2 m-2 hover:bg-slate-800 hover:text-slate-100'>{item}</button> 
+      ))
+    }
+  </div>
+</>
+                      }
+                    </div>
                     </div>
                     
 
