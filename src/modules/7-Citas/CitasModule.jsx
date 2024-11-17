f353f2f190909a8edd0ce8f3405317e695e3eaef
@mpaez1 <mpaez@ikatechsolutions.com>
2024-11-17 13:22:12 -0500

    add-changes

--- src/modules/7-Citas/CitasModule.jsx
+++ src/modules/7-Citas/CitasModule.jsx
@@ -43,7 +43,7 @@ const CitasModule = () => {
           agendaId:"3ef6bc19c90d18bb47063b03fa299dc1c9bb1f1238672084905fcaf8808bb611@group.calendar.google.com"
         },
         { img: "./imgs/BarberoDaniel.jpg",
-          nombre: "Daniel Stiven Cano", 
+          nombre: "Daniel S. Cano", 
           id: 2, 
           diaNoDisponible: "MAR",
           agendaId:"5a5f753eb96bac8155a607114939f484f29f14c98a4e658e782ac5096429e802@group.calendar.google.com"
@@ -596,14 +596,14 @@ const CitasModule = () => {
                                 <svg className="w-2.5 h-2.5 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20"><path fill="currentColor" d="M6 1a1 1 0 0 0-2 0h2ZM4 4a1 1 0 0 0 2 0H4Zm7-3a1 1 0 1 0-2 0h2ZM9 4a1 1 0 1 0 2 0H9Zm7-3a1 1 0 1 0-2 0h2Zm-2 3a1 1 0 1 0 2 0h-2ZM1 6a1 1 0 0 0 0 2V6Zm18 2a1 1 0 1 0 0-2v2ZM5 11v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 11v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 15v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 15v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 11v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM5 15v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM2 4h16V2H2v2Zm16 0h2a2 2 0 0 0-2-2v2Zm0 0v14h2V4h-2Zm0 14v2a2 2 0 0 0 2-2h-2Zm0 0H2v2h16v-2ZM2 18H0a2 2 0 0 0 2 2v-2Zm0 0V4H0v14h2ZM2 4V2a2 2 0 0 0-2 2h2Zm2-3v3h2V1H4Zm5 0v3h2V1H9Zm5 0v3h2V1h-2ZM1 8h18V6H1v2Zm3 3v.01h2V11H4Zm1 1.01h.01v-2H5v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H5v2h.01v-2ZM9 11v.01h2V11H9Zm1 1.01h.01v-2H10v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM9 15v.01h2V15H9Zm1 1.01h.01v-2H10v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM14 15v.01h2V15h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM14 11v.01h2V11h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM4 15v.01h2V15H4Zm1 1.01h.01v-2H5v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H5v2h.01v-2Z"/></svg>
                             </span>
                             <h3 className="mb-1 text-lg font-semibold text-gray-900 ">
-                              Cita para el: 17 de Noviembre de 2024 a las 10:00 AM</h3>
+                              Cita para el: {selectedFecha.day} de {selectedFecha.month} de {selectedFecha.year} a las 10:00 AM</h3>
                             <time className="block mb-3 text-sm font-normal leading-none text-gray-500 ">Duración del servicio: 30 minutos</time>
                             <div className='flex flex-row justify-start items-center gap-2  '>
                               <h3 className="mb-1 text-lg font-semibold text-gray-900 ">
                                 Barbero: </h3> 
-                              <img src="./imgs/BarberoOscar.jpg" alt="Foto del Barbero" className='w-5 h-5 rounded-full'/>
+                              <img src={arrayBarberos[selectedBarbero-1].img} alt="Foto del Barbero" className='w-5 h-5 rounded-full'/>
                               <h3 className="mb-1 text-lg font-semibold text-gray-900 ">
-                                Oscar Rodríguez </h3>  
+                              {arrayBarberos[selectedBarbero-1].nombre} </h3>  
                             </div>
                              </li>
                         <li className="ms-8">
