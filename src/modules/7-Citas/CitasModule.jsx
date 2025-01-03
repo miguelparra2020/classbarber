18cc8b49f37bf3020ff00ca4c4f651b1f25663eb
@mpaez1 <mpaez@ikatechsolutions.com>
2025-01-02 19:52:46 -0500

    add-changes

--- src/modules/7-Citas/CitasModule.jsx
+++ src/modules/7-Citas/CitasModule.jsx
@@ -888,7 +888,7 @@ const CitasModule = () => {
                 <div className="relative bg-white rounded-lg shadow ">
                         <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t ">
                             <h3 className="text-lg font-semibold text-gray-900 ">
-                                Citas agendadas 
+                                Citas agendadas
                             </h3>
                             <button type="button" onClick={toggleModalSearch} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm h-8 w-8 ms-auto inline-flex justify-center items-center " data-modal-toggle="timeline-modal">
                                 <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -897,13 +897,23 @@ const CitasModule = () => {
                                 <span className="sr-only">Close modal</span>
                             </button>
                         </div>
+                        <form class="max-w-sm mx-auto">
+  <label for="website-admin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre de usuario:</label>
+  <div class="flex">
+    <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+      <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+      </svg>
+    </span>
+    <input type="text" id="website-admin" class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bonnie Green" />
+  </div>
+</form>
                         <div className="p-4 md:p-5">
                             <ol className="relative border-s border-gray-200  ms-3.5 mb-4 md:mb-5">                  
                                 
-                                <li className="mb-2 ms-8">
-                                    <h3 className="mb-1 text-lg font-semibold text-gray-900 ">
-                                      Próximamente... </h3>
-                                      </li>
+                                
+
+
                             </ol>
                             <div className='flex flexrow gap-2'>
                                 <button onClick={toggleModalSearch} className="text-white inline-flex w-[20%] justify-center bg-gray-500 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
