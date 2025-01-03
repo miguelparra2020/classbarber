671472e957bcc761b49618260b767ff2f9a58c10
@mpaez1 <mpaez@ikatechsolutions.com>
2025-01-03 10:39:31 -0500

    add-changes

--- src/modules/7-Citas/CitasModule.jsx
+++ src/modules/7-Citas/CitasModule.jsx
@@ -445,11 +445,11 @@ const CitasModule = () => {
               <button onClick={toggleModalSearch} 
               className="bg-customColor8 hover:bg-customColor5 text-white hover:text-gray-800 rounded 
               font-bold py-2 px-4 flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
 </svg> &nbsp;&nbsp;
                  Buscar mis citas 
-                 &nbsp;&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-check" viewBox="0 0 16 16">
+                 &nbsp;&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar-check" viewBox="0 0 16 16">
   <path d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
   <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
 </svg>
@@ -808,22 +808,22 @@ const CitasModule = () => {
                                     <h3 className="mb-1 text-lg font-semibold text-gray-900 ">
                                       Datos para agendar la cita:</h3>
                                       <div>
-                                          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">
+                                          <label for="email" className="block mb-2 text-sm font-medium text-gray-900 ">
                                             Nombre contacto:</label>
                                           <input type="text" value={nameCustomer} name="name" id="name" onChange={(e) => setNameCustomer(e.target.value)}
-                                          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 " placeholder="Nombre usuario" required />
+                                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 " placeholder="Nombre usuario" required />
                                             </div>
                                       <div>
-                                          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">
+                                          <label for="email" className="block mb-2 text-sm font-medium text-gray-900 ">
                                             Correo electrónico:</label>
                                           <input type="email" value={emailCustomer} name="email" id="email" onChange={(e) => setEmailCustomer(e.target.value)}
-                                          class="bg-gray-50 border mb-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 " placeholder="name@gmail.com" required />
+                                          className="bg-gray-50 border mb-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 " placeholder="name@gmail.com" required />
                                         </div>
                                       <div>
-                                          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">
+                                          <label for="email" className="block mb-2 text-sm font-medium text-gray-900 ">
                                             Celular contacto:</label>
                                           <input type="number" value={celCustomer} name="number" id="number" onChange={(e) => setCelCustomer(e.target.value)}
-                                          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 " placeholder="613670695" required />
+                                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 " placeholder="613670695" required />
                                     </div>
                                       
                                 </li>
@@ -897,15 +897,15 @@ const CitasModule = () => {
                                 <span className="sr-only">Close modal</span>
                             </button>
                         </div>
-                        <form class="max-w-sm mx-auto">
-  <label for="website-admin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre de usuario:</label>
-  <div class="flex">
-    <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
-      <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <form className="max-w-sm mx-auto">
+  <label for="website-admin" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre de usuario:</label>
+  <div className="flex">
+    <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+      <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
         <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
       </svg>
     </span>
-    <input type="text" id="website-admin" class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bonnie Green" />
+    <input type="text" id="website-admin" className="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bonnie Green" />
   </div>
 </form>
                         <div className="p-4 md:p-5">
