2e5893cc90dbdb01f060cc255a1bb81381ed47fd
@mpaez1 <mpaez@ikatechsolutions.com>
2025-01-03 18:33:23 -0500

    add-changes

--- src/modules/7-Citas/CitasModule.jsx
+++ src/modules/7-Citas/CitasModule.jsx
@@ -34,6 +34,9 @@ const CitasModule = () => {
     const toggleModalConfirm = () => {setModalConfirmation(!isModalConfirmation)};    
     const [showSearchModal, setShowSearchModal] = useState(false);
     const toggleModalSearch = () => {setShowSearchModal(!showSearchModal)};
+    const [loadingCitasCel, setLoadingCitasCel] = useState(false);
+    const [responseCitasCel, setResponseCitasCel] = useState("");
+    const [citasDisponibles, setCitasDisponibles] = useState([]);
 
     useEffect(() => {
       if(celCustomer.slice(0, 1) === "3"){
@@ -58,6 +61,12 @@ const CitasModule = () => {
         setCelCustomerToSearh(miCelular ? miCelular : "");
       }
     }, [selectedFecha, selectedBarbero, selectedServicio, isModalOpen]);
+
+    const handleSearhCita = (cel) => {
+      setLoadingCitasCel(true);
+      console.log("hola", cel)
+    }
+
     const sumarMinutos = (hora, minutosASumar) => {
       // Convertir la hora en un objeto Date
       const [horaStr] = hora.split(" "); // Se elimina AM/PM, si existe
@@ -438,6 +447,8 @@ const CitasModule = () => {
     toast.error("No se pudo crear la cita");
   }
     };
+
+    
 
 
     return (
@@ -899,26 +910,38 @@ const CitasModule = () => {
                                 <span className="sr-only">Close modal</span>
                             </button>
                         </div>
-                        <form className="max-w-sm mx-auto">
+                        <div className="max-w-sm mx-auto">
   <label for="website-admin" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre del último usuario registrado: {nameCustomer}</label>
   
   <div className="flex">
     <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-phone" viewBox="0 0 16 16">
   <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
   <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
 </svg>
     </span>
     <input type="text" id="website-admin" value={celCustomerToSearh} onChange={(e) => setCelCustomerToSearh(e.target.value)} className="mr-2 rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bonnie Green" />
    
-    <button onClick={toggleModalSearch} className="text-white inline-flex w-[35%] justify-center bg-gray-500 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
-                                Buscar citas
+    <button onClick={() => handleSearhCita(celCustomerToSearh)} disabled={loadingCitasCel}
+    className="text-white inline-flex w-[35%] justify-center bg-gray-500 
+    hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium 
+    rounded-lg text-sm px-5 py-2.5 text-center ">
+                                Buscar citas 
                                 </button>     
   </div>
-</form>
+</div>
                         <div className="p-4 md:p-5">
+                          {loadingCitasCel ? (
+                            <div className="flex justify-center items-center flex-row gap-2">
+                              <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin  fill-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                                  </svg>
+                              Buscando citas... 
+                            </div>
+                          ):null}
                             <ol className="relative border-s border-gray-200  ms-3.5 mb-4 md:mb-5">                  
-                                
+                              
                                 
 
 
